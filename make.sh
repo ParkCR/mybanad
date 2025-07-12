601f11338320ff791aedf9fafe2b08e5741d1fa6
ParkCR <85162830+ParkCR@users.noreply.github.com>
2025-07-12 22:19:08 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# 定义函数计算规则总数
+count_rules() {
+    cat "$1" | grep -Ev "\!|\[|\*" | wc -l
+}
+
 time=$(echo "$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）")
 dnstotal=$(cat jiekouAD.txt | grep -E "^(\|\|)[^\/\^]+\^$" | wc -l)
 echo -e "[Adblock Plus 2.0]\n! Title: 几十KB的轻量规则\n! Homepage: https://github.com/damengzhu/banad\n! by: ParkCR\n! Total Count: $dnstotal\n! Update Time:$time" >dnslist.txt
@@ -12,4 +18,15 @@ sed -i "s/HOSTCOUNT/$hosttotal/" hosts.txt
 sed -i "s/! Update Time:.*/! Update Time: $time/g" jiekouAD.txt
 total=$(cat jiekouAD.txt | grep -v "^!" | wc -l)
 sed -i "s/! Total Count:.*/! Total Count: $total/g" jiekouAD.txt
+
+# 生成 mybanad.txt 文件
+mybanadtotal=$(count_rules dnslist.txt)
+echo -e "#Title: 几十KB的轻量规则\n#Homepage: https://github.com/damengzhu/banad\n#by: ParkCR\n#Total Count: $mybanadtotal\n#Update Time: $time" >mybanad.txt
+cat dnslist.txt | grep -Ev "\!|\[|\*" | sed -e 's/||//g' -e "s/\^/ = 0.0.0.0/g" | sort -u >>mybanad.txt
+
+# 生成 Banad-RULE-SET.txt 文件
+banadtotal=$(count_rules dnslist.txt)
+echo -e "#Title: 几十KB的轻量规则\n#Homepage: https://github.com/damengzhu/banad\n#by: ParkCR\n#Total Count: $banadtotal\n#Update Time: $time" >Banad-RULE-SET.txt
+cat dnslist.txt | grep -Ev "\!|\[|\*" | sed -e 's/||//g' -e "s/\^//g" | awk '{print "DOMAIN," $0}' | sort -u >>Banad-RULE-SET.txt
+
 exit 0
